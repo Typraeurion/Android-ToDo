4109c263fe35042a721f6416f0b297f12dce1a73
Trevin Beattie <trevin@xmission.com>
2025-02-17 22:09:02 +0000

    * Migrated the project from Eclipse to Android Studio (2.3.3)   following the recommended directory structure.  Added Gradle 3.5   (with wrapper). * Include jars for the older android gradle plugin which is not   available in either Maven Central nor Google's Maven repository. * Increased the target SDK version from 15 (Ice Cream Sandwich) to 22   (Lollipop).  The code currently builds and runs on Android 8 (Oreo)   in emulation; not tested on Android 2 (Froyo) due to lack of   emulator support for arm on an x86-64 host. * Fixed some errors in localized string identifiers. * When updating preferences, use .apply() rather than .commit() (as   recommended by the code inspector). * Updated CategoryFilterCursorAdapter and NoSelectionCursorAdapter to   conform to a change in API 19 in which Spinner requires the adapter   to have only 1 view type. * Refactored anonymous instances of   LoaderManager.LoaderCallbacks<Cursor> into separate class files.   Modified the anonymous CursorLoader instances to rely more on their   superclass methods to fix an “attempt to use a closed cursor” bug   that popped up in testing target SDK 22. * Fixed a number of minor warnings and Javadoc errors in the code. * Replaced HashMaps with SparseArrays (recommended by the code   inspector) in a few cases where the key is a number and the maps are   small. * Updated the Building ToDo document to include the current build   environment, updated the paths to the icon source files, and added a   new section on building the application package. * In the main ToDo document, replaced the old SourceForge link with a   link to the newly created GitHub project.  Added another outstanding   bug to the list where exporting a large data set can fail.

---
+++ app/src/main/java/com/xmission/trevin/android/todo/ItemLoaderCallbacks.java
@@ -0,0 +1,137 @@
+package com.xmission.trevin.android.todo;
+
+import static com.xmission.trevin.android.todo.ToDoListActivity.ITEM_PROJECTION;
+import static com.xmission.trevin.android.todo.ToDoListActivity.TPREF_SORT_ORDER;
+
+import android.annotation.TargetApi;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * As of Honeycomb (API 11), the application needs to re-initialize
+ * the item cursor whenever the activity is restarted.
+ * This class provides the callbacks for the item loader manager.
+ * Used when ToDoListActivity is created.
+ *
+ * @deprecated as of Pie (API 28)
+ */
+@TargetApi(11)
+class ItemLoaderCallbacks
+        implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final String TAG = "ItemLoaderCallbacks";
+
+    private final ToDoListActivity listActivity;
+    private final SharedPreferences sharedPrefs;
+
+    // Used to map To Do entries from the database to views
+    private final ToDoCursorAdapter itemAdapter;
+
+    /** The URI by which we were started for the To-Do items */
+    private final Uri todoUri;
+
+    ItemLoaderCallbacks(ToDoListActivity activity,
+                        SharedPreferences prefs,
+                        ToDoCursorAdapter adapter,
+                        Uri uri) {
+        listActivity = activity;
+        sharedPrefs = prefs;
+        itemAdapter = adapter;
+        todoUri = uri;
+    }
+
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Log.d(TAG, ".onCreateLoader");
+        CursorLoader loader = new CursorLoader(listActivity,
+                // The selection and sort order parameters
+                // are overridden in loadInBackground
+                todoUri, ITEM_PROJECTION, null, null,
+                ToDo.ToDoItem.DEFAULT_SORT_ORDER) {
+
+            //private Cursor myCursor = null;
+
+            @Override
+            public Cursor loadInBackground() {
+                Log.d(TAG, ".CursorLoader.loadInBackground");
+                /*
+                if (myCursor != null)
+                    myCursor.close();
+                */
+                int selectedSortOrder = sharedPrefs.getInt(TPREF_SORT_ORDER, 0);
+                if ((selectedSortOrder < 0) ||
+                        (selectedSortOrder >= ToDo.ToDoItem.USER_SORT_ORDERS.length)) {
+                    sharedPrefs.edit().putInt(TPREF_SORT_ORDER, 0).apply();
+                    selectedSortOrder = 0;
+                }
+                setSelection(listActivity.generateWhereClause());
+                setSortOrder(ToDo.ToDoItem.USER_SORT_ORDERS[selectedSortOrder]);
+                /*
+                myCursor = listActivity.getContentResolver().query(todoUri,
+                        ITEM_PROJECTION, listActivity.generateWhereClause(), null,
+                        ToDo.ToDoItem.USER_SORT_ORDERS[selectedSortOrder]);
+                return myCursor;
+                */
+                return super.loadInBackground();
+            }
+
+            @Override
+            public void cancelLoadInBackground() {
+                Log.d(TAG, ".CursorLoader.cancelLoadInBackground");
+                super.cancelLoadInBackground();
+            }
+
+            @Override
+            public void deliverResult(Cursor cursor) {
+                Log.d(TAG, String.format(".CursorLoader.deliverResult(%s)",
+                        (cursor == null) ? "null"
+                                : cursor.isClosed() ? "closed cursor"
+                                : "open cursor"));
+                super.deliverResult(cursor);
+            }
+
+            @Override
+            protected void onStartLoading() {
+                Log.d(TAG, ".CursorLoader.onStartLoading");
+                super.onStartLoading();
+            }
+
+            @Override
+            public void onCanceled(Cursor cursor) {
+                Log.d(TAG, String.format(".CursorLoader.onCanceled(%s)",
+                        (cursor == null) ? "null"
+                                : cursor.isClosed() ? "closed cursor"
+                                : "open cursor"));
+                super.onCanceled(cursor);
+            }
+
+            @Override
+            protected void onStopLoading() {
+                Log.d(TAG, ".CursorLoader.onStopLoading");
+                super.onStopLoading();
+                /*
+                if (myCursor != null)
+                    myCursor.close();
+                myCursor = null;
+                */
+            }
+        };
+        return loader;
+    }
+
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        Log.d(TAG, ".onLoadFinished");
+        itemAdapter.swapCursor(data);
+    }
+
+    public void onLoaderReset(Loader<Cursor> loader) {
+        Log.d(TAG, ".onLoaderReset");
+        itemAdapter.swapCursor(null);
+    }
+
+}
